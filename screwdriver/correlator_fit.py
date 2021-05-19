67286f1b8eb828ad8783432efd5e4fdc7435e10e
Tomas L. Howson <tomas.howson@adelaide.edu.au>
2021-05-19 10:07:26 +0930

    Added effective mass calculator

--- screwdriver/correlator_fit.py
+++ screwdriver/correlator_fit.py
@@ -1,4 +1,5 @@
 import numpy as np
+from . import misc_functions as mf
 
 
 def G2_1exp_func(x, A0, m0):
@@ -9,8 +10,8 @@ def G2_2exp_func(x, A0, m0, A1, m1):
     return A0 * np.exp(-m0 * x) + A1 * np.exp(-m1 * x)
 
 
-def G2_1exp_func_middle(x, A0, m0):
-    return A0 * (np.exp(-m0 * x) + np.exp(-m0 * (48 - x)))
+def G2_1exp_func_middle(x, A0, m0, Nt):
+    return A0 * (np.exp(-m0 * x) + np.exp(-m0 * (Nt - x)))
 
 
 def G3_1exp_func(x, B00, A0, m0):
@@ -38,4 +39,14 @@ def G3_2exp_func(x, B00, B10, B11, A0, m0, A1, m1):
             + B11 * np.exp(-Δm * t)
         )
     )
+    return result
+
+
+def effective_mass(t, G2, a, Nt, time_axis=1):
+    t_adv = mf.wherein(t, (t + a) % Nt)
+    if np.any(t_adv.mask):
+        raise ValueError(
+            "Correlator must be defined at 't+a % Nt' for every point in 't'"
+        )
+    result = (1 / a) * np.log(np.abs(G2 / G2.take(t_adv, axis=time_axis)))
     return result
