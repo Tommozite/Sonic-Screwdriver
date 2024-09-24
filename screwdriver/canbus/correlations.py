53c82a5519e022c9df7cfa1e0d8de0b42723dcd4
Tomas Howson <tomas.howson@adelaide.edu.au>
2024-09-24 20:40:30 +0930

    added set of correlation calculators

---
+++ screwdriver/canbus/correlations.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from .capture import Capture
+
+
+def pearson(X, Y):
+    cov = np.cov(X, Y)
+    std_X = np.std(X)
+    std_Y = np.std(Y)
+    coeff = cov / (std_X * std_Y)
+    return coeff
+
+
+def point_biserial(X, Y):
+    M1 = np.mean(X[Y], axis=0)
+    M0 = np.mean(X[~Y], axis=0)
+    sn = np.std(X)
+    n = len(Y)
+    n1 = np.sum(Y)
+    n0 = n - n1
+    coeff = ((M1 - M0) / sn) * np.sqrt(n1 * n0 / n**2)
+    return coeff
+
+
+def smc(X, Y):
+    M11 = np.sum(np.logical_and(X, Y))
+    M01 = np.sum(np.logical_and(~X, Y))
+    M10 = np.sum(np.logical_and(X, ~Y))
+    M00 = np.sum(np.logical_and(~X, ~Y))
+    coeff = (M00 + M11) / (M00 + M10 + M01 + M11)
+    return coeff
